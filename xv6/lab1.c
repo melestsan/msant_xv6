591a9d068725aef1a7d4d172e0aef9338020aa2d
melsantos <msant035@ucr.edu>
2017-11-02 16:04:00 -0700

    Modified lab1.c

--- xv6/lab1.c
+++ xv6/lab1.c
@@ -4,24 +4,24 @@
 int main(int argc, char *argv[])
 {
 	
-	int exitWait(void);
-	int waitPid(void);
-	int PScheduler(void);
+  int exitWait(void);
+  int waitPid(void);
+  int PScheduler(void);
 
   printf(1, "\n This program tests the correctness of your lab#1\n");
   
   if (atoi(argv[1]) == 1)
-	exitWait();
+    exitWait();
   else if (atoi(argv[1]) == 2)
-	waitPid();
+    waitPid();
   else if (atoi(argv[1]) == 3)
-	PScheduler();
+    PScheduler();
   else 
-   printf(1, "\ntype \"lab1 1\" to test exit and wait, \"lab1 2\" to test waitpid and \"lab1 3\" to test the priority scheduler \n");
+    printf(1, "\ntype \"lab1 1\" to test exit and wait, \"lab1 2\" to test waitpid and \"lab1 3\" to test the priority scheduler \n");
   
-    // End of test
-	 exit(0);
- }
+  // End of test
+  exit(0);
+}
   
   
 int exitWait(void) {
@@ -34,25 +34,18 @@ int exitWait(void) {
   for (i = 0; i < 2; i++) {
     pid = fork();
     if (pid == 0) { // only the child executed this code
-      if (i == 0)
-      {
+      if (i == 0) {
         printf(1, "\nThis is child with PID# %d and I will exit with status %d\n", getpid(), 0);
         exit(0);
-      }
-      else
-      {
+      } else {
 	printf(1, "\nThis is child with PID# %d and I will exit with status %d\n" ,getpid(), -1);
         exit(-1);
       }    
-    } 
-    else if (pid > 0) 
-    { // only the parent exeecutes this code
+    } else if (pid > 0) { // only the parent exeecutes this code
       ret_pid = wait(&exit_status);
       printf(1, "\n This is the parent: child with PID# %d has exited with status %d\n", ret_pid, exit_status);
-    } 
-    else  // something went wrong with fork system call
-    {  
-	  printf(2, "\nError using fork\n");
+    } else { // something went wrong with fork system call
+      printf(2, "\nError using fork\n");
       exit(-1);
     }
   }
@@ -66,7 +59,7 @@ int waitPid(void){
   int pid_a[5]={0, 0, 0, 0, 0};
  // use this part to test wait(int pid, int* status, int options)
 
- printf(1, "\n  Step 2: testing waitpid(int pid, int* status, int options):\n");
+  printf(1, "\n  Step 2: testing waitpid(int pid, int* status, int options):\n");
 
   for (i = 0; i <5; i++) {
     pid_a[i] = fork();
@@ -102,7 +95,7 @@ int waitPid(void){
   }
       
       
-  int PScheduler(void) {
+int PScheduler(void) {
 		 
     // use this part to test the priority scheduler. Assuming that the priorities range between range between 0 to 63
     // 0 is the highest priority. All processes have a default priority of 20 
@@ -118,29 +111,35 @@ int waitPid(void){
     for (i = 0; i <  3; i++) {
 	pid = fork();
 	if (pid > 0 ) {
-		continue;}
-	else if ( pid == 0) {
-//		printf(1, "\n Hello! this is child# %d and I will change my priority to %d \n",getpid(),60-20*i);
-		setpriority(60-20*i);	
-		for (j=0;j<50000;j++) {
-			for(k=0;k<10000;k++) {
-				asm("nop"); }}
-		printf(1, "\n child# %d with priority %d has finished! \n",getpid(),60-20*i);		
-		exit(0);
+	  continue;
+	} else if ( pid == 0) {
+          //printf(1, "\n Hello! this is child# %d and I will change my priority to %d \n",getpid(),60-20*i);
+	  setpriority(60-20*i);	
+	  for (j=0;j<50000;j++) {
+		for(k=0;k<10000;k++) {
+			asm("nop"); 
+		}
+	  }
+	  printf(1, "\n child# %d with priority %d has finished! \n",getpid(),60-20*i);		
+	  exit(0);
+        } else {
+	  printf(2," \n Error \n");
+	  exit(-1);
         }
-        else {
-			printf(2," \n Error \n");
-			exit(-1);
-        }
-	}
+    }
 
-	if(pid > 0) {
-		for (i = 0; i <  3; i++) {
-			ret_pid = wait(&exit_status);
-			//printf(1,"\n This is the parent: child with PID# %d has finished with status %d \n",ret_pid,exit_status);
-			}
-                     printf(1,"\n if processes with highest priority finished first then its correct \n");
-}
+    if(pid > 0) {
+	for (i = 0; i <  3; i++) {
+	    ret_pid = wait(&exit_status);
+	    if(ret_pid < 0) {
+		printf(1, "wait error\n");
+		exit(-1);
+	    }
+	    //printf(1,"\n This is the parent: child with PID# %d has finished with status %d \n",ret_pid,exit_status);
+        }
+        printf(1,"\n if processes with highest priority finished first then its correct \n");
+    }
 			
-	return 0;}
+  return 0;
+}
 
